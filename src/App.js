11480037b9c909b55d4450c381bba83a7b247df6
electrone901 <carbajalluis@rocketmail.com>
2021-07-10 15:12:46 -0400

    Setup: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Initial set up</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout"><h1>checkout</h1> </Route>
+          <Route path="/login"><h1>login</h1> </Route>
+          <Route path="/"><h1>login</h1> </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
