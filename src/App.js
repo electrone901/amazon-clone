f9bc79a93668a2a00221222a8dbb6967d11b144a
electrone901 <carbajalluis@rocketmail.com>
2021-07-10 18:30:06 -0400

    Added: Nav component and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Header'
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/checkout"><h1>checkout</h1> </Route>
-          <Route path="/login"><h1>login</h1> </Route>
-          <Route path="/"><h1>login</h1> </Route>
+          <Route path="/checkout">
+            <h1>checkout</h1>{' '}
+          </Route>
+          <Route path="/login">
+            <h1>login</h1>{' '}
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
         </Switch>
       </div>
     </Router>
