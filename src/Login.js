9c239df6d1765be7095b48364c7061f3c442c3d2
electrone901 <carbajalluis@rocketmail.com>
2021-07-12 20:06:34 -0400

    Built: Login page,  enable firebase authentication and added  logic to the navbar to reflex the user presence

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import './Login.css'
+import { auth } from './firebase'
+
 function Login() {
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = (event) => {
+    event.preventDefault()
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // logged in successfully then redirect to main page
+        history.push('/')
+      })
+      .catch((e) => alert(e.message))
+  }
+  const register = (event) => {
+    event.preventDefault()
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        //  created an user and logged n redirect to main page
+        history.push('/')
+      })
+      .catch((e) => alert(e.message))
+  }
   return (
-    <div>
-      <h1>Login component</h1>
+    <div className="login">
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+          className="login__logo"
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form action="">
+          <h5>E-mail</h5>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+          />
+          <h5>Password</h5>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <button
+            value={password}
+            onClick={login}
+            type="submit"
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to Amazon's Conditions of Use & Sale. Please
+          see our Privacity notice, our Cookies Notice and our Interest-Based
+          Ads Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   )
 }
